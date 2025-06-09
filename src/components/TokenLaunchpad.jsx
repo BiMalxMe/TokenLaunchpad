918ac0f87ecd41732569905b6fc3f34185c17b93
BiMalxMe <journeyxbimal@gmail.com>
2025-06-09 06:28:10 +0545

    CreatingAToken

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,12 +1,41 @@
+import { createInitializeAccountInstruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 
-export function TokenLaunchpad() {
+export  function TokenLaunchpad() {
+    const {connection} = useConnection();
+    const wallet = useWallet();
 
     //scrap the input fields data
-    function createToken() {
+    async  function createToken() {
         const name = document.getElementById('name').value;
         const symbol = document.getElementById('symbol').value;
         const image = document.getElementById('image').value;
         const initialSupply = document.getElementById('initialSupply').value;
+
+        //get a minimum acmount needed to mint a token
+        const lamports =await getMinimumBalanceForRentExemptMint(connection)
+
+        let keypair = Keypair.generate();
+        const transaction = new Transaction().add(
+            //creating a new account in the blockchain
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: keypair.publicKey,
+                space : MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID,
+            }),
+
+            // 6 here is representing a decimal
+            //4th arg is optional i.e freezeauthority
+            createInitializeAccountInstruction(keypair.publicKey, 6, wallet.publicKey,wallet.publicKey, TOKEN_PROGRAM_ID),
+        );
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+        transaction.partialSign(keypair);
+       let response  = await wallet.sendTransaction(transaction)
+        console.log(response)
     }
 
     return  <div style={{
